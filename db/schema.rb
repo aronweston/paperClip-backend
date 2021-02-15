c89c8ddebfbd99908b228179b825326eb00db155
aronweston <aron@firststepdigital.com.au>
2021-02-15 17:00:07 +1100

    add profile image to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_14_023527) do
+ActiveRecord::Schema.define(version: 2021_02_15_055433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_02_14_023527) do
     t.text "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "profile_photo"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.destroy_all
-u1 = User.create :username => 'ac', :first_name => 'andre', :last_name => 'caleb', :email => 'ac@ga.co', :password => 'chicken', :city => 'Sydney', :admin => true
-u2 = User.create :username => 'purplehippo', :first_name => 'bill', :last_name => 'murray', :email => 'bmy@ga.co', :password => 'chicken', :city => "Melbourne"
-u3 = User.create :username => 'thegrinch',:first_name => 'craig', :last_name => 'gibson', :email => 'cg@ga.co', :password => 'chicken', :city => "Perth"
+u1 = User.create :username => 'ac', :first_name => 'andre', :last_name => 'caleb', :email => 'ac@ga.co', :password => 'chicken', :city => 'Sydney', :profile_photo => "https://randomuser.me/api/portraits/men/71.jpg", :admin => true
+u2 = User.create :username => 'purplehippo', :first_name => 'bill', :last_name => 'murray', :email => 'bmy@ga.co', :password => 'chicken', :city => "Melbourne", :profile_photo => "https://randomuser.me/api/portraits/men/33.jpg"
+u3 = User.create :username => 'thegrinch',:first_name => 'craig', :last_name => 'gibson', :email => 'cg@ga.co', :password => 'chicken', :city => "Perth", :profile_photo => "https://randomuser.me/api/portraits/women/71.jpg"
 puts "#{User.count} users"

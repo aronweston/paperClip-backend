966a2405634af8b58d0cf31582792cc04db88347
Andre Anggono <andre.anggono@hotmail.com>
2021-02-18 17:11:34 +1100

    Testing cors with public ip

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     credentials: true
   end
 
-  # allow do
-  #   origins 'https://localhost:3001' # update with heroku domain
-  #   resource '*',
-  #   :headers => :any,
-  #   :methods => %i( get post put patch delete options head ), # %i converts it to symbols
-  #   credentials: true
-  # end
+  allow do
+    origins 'https://124.168.75.194:3001' # update with heroku domain
+    resource '*',
+    :headers => :any,
+    :methods => %i( get post put patch delete options head ), # %i converts it to symbols
+    credentials: true
+  end
 end

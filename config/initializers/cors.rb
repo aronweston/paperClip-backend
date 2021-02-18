769acea07225a68f64066c2e0a0aecb61b9fdea1
Andre Anggono <andre.anggono@hotmail.com>
2021-02-19 01:24:35 +1100

    Modify cors entries for netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'http://localhost:3001' # this actually needs an explicit list of real URLs on Heroku
-    resource '*',
-    :headers => :any,
-    :methods => %i( get post put patch delete options head ), # %i converts it to symbols
-    credentials: true
-  end
+  # allow do
+  #   origins 'http://localhost:3001' # this actually needs an explicit list of real URLs on Heroku
+  #   resource '*',
+  #   :headers => :any,
+  #   :methods => %i( get post put patch delete options head ), # %i converts it to symbols
+  #   credentials: true
+  # end
 
   allow do
     origins 'https://justclipit.netlify.app/' # update with heroku domain
